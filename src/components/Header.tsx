87da5231cd55bbeae28982f4fe30cf3fa9f0a0bf
Lavlesh <lavleshdubey90@gmail.com>
2026-04-02 21:54:05 +0530

    Basic Layout Made

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = () => {
+  return (
+    <header className='border border-base-300 w-full rounded-box p-5 bg-base-200'>
+        <h1>Header</h1>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar: React.FC = () => {
+  return (
+    <aside className="w-80 border border-base-300 bg-base-200 p-4 rounded-box">
+      <h2>Sidebar</h2>
+    </aside>
+  )
+}
+
+export default Sidebar;
